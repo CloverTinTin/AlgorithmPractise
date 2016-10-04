6022be1c6aa983405cfb66750ef542e8b6728e47
Cai <cyqmailbox@gmail.com>
2016-10-04 22:44:05 +0900

    commit eight queens solution code.

---
+++ Others/eightQueenProblem/eightQueens.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void eightQueens(int queenAmount, int *counter, int rowNum, int solution[])
+{
+    if(rowNum >= queenAmount)
+    {
+	++(*counter);
+	for(int i = 0; i < queenAmount; ++i)
+	    cout << solution[i] << " ";
+	cout << endl;
+	return;
+    }
+
+   for(int column = 0; column < queenAmount; ++column)	   
+   {
+       int row;
+       for(row = 0; row < rowNum; ++row)
+	   if(solution[row] == column || (row- rowNum == solution[row] - column) || (row - rowNum == column - solution[row]))
+	       break;
+       if(row == rowNum)
+       {
+	   solution[rowNum] = column;
+	   eightQueens(queenAmount, counter, rowNum + 1, solution);
+       } 
+   }
+}
+
+int main()
+{
+    int counter = 0;
+    int solution[100];
+
+    eightQueens(8, &counter, 0, solution);
+    cout << counter << endl;
+    return 0;
+}
